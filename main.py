4f80d477adcb62f63029997ed2742531dd86854d
Chakib Rached, Pharm.D <chakibrachedi@pm.me>
2022-10-17 12:44:13 +0100

    init commit
    added codebase

---
+++ main.py
@@ -0,0 +1,62 @@
+import requests
+from datetime import datetime
+import smtplib, time
+
+MY_LAT = 35.190922  # Your latitude
+MY_LONG = -0.635230  # Your longitude
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
+
+
+# Your position is within +5 or -5 degrees of the ISS position.
+def is_in_position():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+    if MY_LONG - 5 <= iss_longitude <= MY_LONG + 5 and MY_LAT - 5 <= iss_latitude <= MY_LAT + 5:
+        return True
+
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+time_now = datetime.now()
+time_now = int(time_now.strftime('%I'))
+
+
+def is_it_dark():
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+    if sunset <= time_now <= sunrise:
+        return True
+
+
+while True:
+    time.sleep(60)
+    if is_it_dark() and is_in_position():
+        with smtplib.SMTP("smtp.gmail.com") as con:
+            con.starttls()
+            con.login(user="chakibtester@gmail.com", password="testerCR1902")
+            con.sendmail(from_addr="chakibtester@gmail.com", to_addrs="chakibrachedi@pm.me", msg="Subject: ISS is over you!\n\nLook Up!")
